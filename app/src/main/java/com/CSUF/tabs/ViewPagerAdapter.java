59a9fd49700db90dacc2a81e3b4a08567892d02a
hmmousa <hlu-2@live.com>
2016-05-06 03:20:29 -0700

    Putting Activities and layouts in folders for easy access and organization

--- app/src/main/java/com/CSUF/tabs/ViewPagerAdapter.java
+++ app/src/main/java/com/CSUF/tabs/ViewPagerAdapter.java
@@ -32,18 +32,18 @@ public class ViewPagerAdapter extends FragmentStatePagerAdapter {
         Log.e("position is ::: ", ""+position);
         if(position == 0) // if the position is 0 we are returning the First tab
         {
-                Tab1 tab1 = new Tab1();
+                NearbyTab tab1 = new NearbyTab();
             return tab1;
 
         }
         else if(position == 1)            // As we are having 2 tabs if the position is now 0 it must be 1 so we are returning second tab
         {
-             Tab3  tab3 = new Tab3();
+             MapTab tab3 = new MapTab();
             return tab3;
 
         }
         else{
-               Tab2 tab2 = new Tab2();
+               NotificationTab tab2 = new NotificationTab();
             return tab2;
         }
 
